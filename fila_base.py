57318d48448a0be57f6b0d93fdec8898d4556ce8
Allex Vissoci <allex@mtexpert.com.br>
2023-07-22 10:59:53 -0300

    criando fila base

---
+++ fila_base.py
@@ -0,0 +1,11 @@
+class FilaBase:
+    codigo = 0
+    fila: list = []
+    clientes_atendidos: list = []
+    senha_atual = None
+
+    def reseta_fila(self) -> None:
+        if self.codigo >= 100:
+            self.codigo = 0
+        else:
+            self.codigo += 1
